82411d42363d31baccbfb717b39bc58216f608dc
Luis Cabezon Manchado <39278178+lucabem@users.noreply.github.com>
2019-02-05 13:26:31 +0100

    Add files via upload

---
+++ Cliente/Principal.java
@@ -0,0 +1,13 @@
+package Cliente;
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		InterfazGrafica ig = new InterfazGrafica();
+		ig.mostrarInterfaz();
+		while(ig.isActive()) {
+			
+		}
+	}
+
+}
